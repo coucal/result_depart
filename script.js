913301c0b7bae71372e7bed0e81cad3326a3ea8a
docsa <alain.couchot@gmail.com>
2016-06-26 22:07:39 +0200

    Avec factory mais erreur sur docsa

--- script.js
+++ script.js
@@ -1,11 +1,51 @@
 console.log("Angular");
-  var app=angular.module('stateapp',[]);
-  app.controller('statecontroller',['$scope','$http',function($scope,$http) {
-    console.log("scope");
-    $http.get("http://www.citysearch-api.com/fr/departement?login=elections-app&apikey=so434425c17043e56ca7800d728bd5ca773977c3f1").then(function(response) {
-      console.log(response.data);
-      $scope.states=response.data.results;
-    },function(err) {
-      console.log(err);
+var app = angular.module('stateapp', []);
+app.controller('statecontroller', function($scope, LocationService) {
+        $scope.CountryId = null;
+        $scope.StateId = null;
+        $scope.CountryList = null;
+        $scope.StateList = null;
+        $scope.StateTextToShow = "Select State";
+        $scope.result="";
+
+        LocationService.GetCountry().then(function(d) {
+            console.log(d.data)
+            $scope.CountryList = d.data.results;
+        }, function(error) {
+            console.log(error);
+            alert("error !");
+        });
+
+        $scope.GetState = function() {
+            $scope.StateId = null;
+            $scope.StateList = null;
+            $scope.StateTextToShow = "Please wait..";
+
+            LocationService.GetState($scope.CountryId).then(function(d) {
+                scope.StateList = d.data;
+                console.log(d.data)
+                $scope.StateTextToShow = "Select State";
+            }, function(error) {
+                console.log(error);
+                alert("error !");
+            });
+        }
+        $scope.ShowResult = function() {
+            $scope.Result = "Selected " + $scope.countryId + " State Id:" + $scope.StateId;
+        }
+    })
+    .factory('LocationService', function($http) {
+        var fac = {};
+
+        fac.GetCountry = function() {
+            console.log("GetCountry");
+            return $http.get("http://www.citysearch-api.com/fr/departement?login=elections-app&apikey=so434425c17043e56ca7800d728bd5ca773977c3f1");
+        }
+
+        fac.GetState = function(countryId) {
+            console.log("GetState");
+            return $http.get("http://docsa.fr:8080/depart/cantons/" + countryId);
+        }
+
+        return fac;
     });
-  }]);
