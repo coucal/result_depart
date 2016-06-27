64dc4e0c16cf0c037f9c7834a8583114881375d3
Couchot <Alain.Couchot@mulhouse.fr>
2016-06-27 21:24:14 +0200

    Changement de noms et ajout feuille de style

--- script.js
+++ script.js
@@ -1,50 +1,55 @@
 console.log("Angular");
 var app = angular.module('stateapp', []);
 app.controller('statecontroller', function($scope, LocationService) {
-        $scope.CountryId = null;
-        $scope.StateId = null;
-        $scope.CountryList = null;
-        $scope.StateList = null;
-        $scope.StateTextToShow = "Select State";
+        $scope.DepartId = null;
+        $scope.CantonId = null;
+        $scope.DepartList = null;
+        $scope.CantonList = null;
+        $scope.CantonTextToShow = "Choisir un canton";
         $scope.result="";
 
-        LocationService.GetCountry().then(function(d) {
+        LocationService.GetDepart().then(function(d) {
             console.log(d.data)
-            $scope.CountryList = d.data.results;
+            $scope.DepartList = d.data.results;
         }, function(error) {
             console.log(error);
             alert("error !");
         });
 
-        $scope.GetState = function() {
-            $scope.StateId = null;
-            $scope.StateList = null;
-            $scope.StateTextToShow = "Please wait..";
+        $scope.GetCanton = function() {
+            $scope.CantonId = null;
+            $scope.CantonList = null;
+            $scope.CantonTextToShow = "Un instant...";
 
-            LocationService.GetState($scope.CountryId).then(function(d) {
-                $scope.StateList = d.data;
+            LocationService.GetCanton($scope.DepartId).then(function(d) {
+                $scope.CantonList = d.data;
                 console.log(d.data)
-                $scope.StateTextToShow = "Select State";
+                $scope.CantonTextToShow = "Choisir un canton";
             }, function(error) {
                 console.log(error);
                 alert("error !");
             });
         }
         $scope.ShowResult = function() {
-            $scope.Result = "Selected " + $scope.countryId + " State Id:" + $scope.StateId;
+            $scope.Result = "Selected " + $scope.DepartId + " Canton Id:" + $scope.CantonId;
         }
     })
     .factory('LocationService', function($http) {
         var fac = {};
 
-        fac.GetCountry = function() {
-            console.log("GetCountry");
+        fac.GetDepart = function() {
+            console.log("GetDepart");
             return $http.get("http://www.citysearch-api.com/fr/departement?login=elections-app&apikey=so434425c17043e56ca7800d728bd5ca773977c3f1");
         }
 
-        fac.GetState = function(countryId) {
-            console.log("GetState");
-            return $http.get("http://docsa.fr:8080/depart/cantons/" + countryId);
+        fac.GetCanton = function(DepartId) {
+            console.log("GetCanton");
+            return $http.get("http://docsa.fr:8080/depart/cantons/" + DepartId);
+        }
+
+        fac.GetResult = function(DepartId) {
+            console.log("GetCanton");
+            return $http.get("http://docsa.fr:8080/depart/result/" + DepartId);
         }
 
         return fac;
